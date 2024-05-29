db590bae1156e1a923a20bdbad903e7f643ab2bf
ggormsby <163230733+ggormsby@users.noreply.github.com>
2024-05-29 13:22:04 +1000

    cloud-computing-experiment

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ import base64
 app = Flask(__name__)
 
 ### TO EDIT ###
-sql_host = '34.87.225.240' 
-sql_connection_name='ultimate-bit-387412:australia-southeast1:googlecloudsql'
+sql_host = '35.244.85.237' 
+sql_connection_name='awesome-nimbus-424801-j4:australia-southeast1:googlecloudsql'
 sql_port = 3306
 sql_database = 'emotions'
 sql_user = 'root'
